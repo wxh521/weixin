c7ab8546dae396d3c2f0f5d00e0b2173db12af47
zhaobin <zhaobin@deepin.deepin-domain>
2014-06-26 17:07:59 +0800

    add

---
+++ public/list.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>功能列表</title>
+    <link href="/front/css/bootstrap.min.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+      <script src="/front/js/html5shiv.js"></script>
+      <script src="/front/js/respond.min.js"></script>
+    <![endif]-->
+    <script src="/front/js/jquery.min.js"></script>
+    <script src="/front/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+      <div class="list-group">
+  <a href="/helper/weather.php" class="list-group-item">
+    <h4 class="list-group-item-heading">1.天气查询</h4>
+    <p class="list-group-item-text">输入城市+天气预报，例如大连天气预报，即可得到最近4天的气象信息</p>
+  </a>
+    
+    <a href="/helper/delivery.php" class="list-group-item">
+    <h4 class="list-group-item-heading">2.快递查询</h4>
+    <p class="list-group-item-text">输入快递名称+运单号可以查询快递信息</p>
+  </a>
+    
+    <a href="/helper/translate.php" class="list-group-item">
+    <h4 class="list-group-item-heading">3.实时翻译</h4>
+    <p class="list-group-item-text">支持多种语言的实时转换</p>
+  </a>
+</div>
+  </body>
+</html>
+
