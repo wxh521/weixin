a6c04510b98ffc6bca8fd42e66a4a08477adb5b9
zhaobin <zhaobin@deepin.deepin-domain>
2014-06-27 12:51:41 +0800

    add

--- public/index.php
+++ public/index.php
@@ -1,13 +1,10 @@
-<?php
-header('Content-Type: text/html; charset=UTF-8');
-?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>功能列表</title>
+    <title>生活好帮手</title>
     <link href="/front/css/bootstrap.min.css" rel="stylesheet">
     <link href="/front/css/life.css" rel="stylesheet">
     <!--[if lt IE 9]>
